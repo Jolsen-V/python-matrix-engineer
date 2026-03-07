670ceec1ac17caf30c7c81ab9266bb6d7827a9c3
Jolsen-V <jvidalmilla03@gmail.com>
2026-03-07 16:12:49 -0500

    ✅ feat(M01): practica 01 - variables

---
+++ practica_1.py
@@ -0,0 +1,23 @@
+# Desafio 01: variables con información
+#============================================
+
+print("my first practice ")
+
+browser = " my first navegation for the web"
+ip = 12273749934934
+citys = "peruvian"
+study = True
+goals = "engineer"
+sucess = 100.1 
+
+#Verificando los tipos 
+#=================================================
+print((type(browser)))
+print(type(ip))
+print(type(citys))
+print(type(study))
+print(type(goals))
+print(type(sucess))
+
+
+
