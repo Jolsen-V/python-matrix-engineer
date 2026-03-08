db21597901ba6a28a5c1235bf99fd269bfffd91d
Jolsen-V <jvidalmilla03@gmail.com>
2026-03-08 11:42:48 -0400

    ✅ feat(M01): practica 03 - entrevista de trabajo con input()

--- practica-03.py
+++ practica-03.py
@@ -11,14 +11,15 @@ age_actual = 2026 - age_birth
 experience = input("Do you have work experience?: ")
 address = input("Where do you live now?: ")
 position = input("what position would you like to apply for?: ")
-remuneration = input("How much would you like to earn?: ")
+remuneration = float(input("How much would you like to earn?: "))
+print()
 
 print(f"""Hello,{name} welcome to company.
 Tell me your year of birth: {age_birth}.
 So his current age is: {age_actual}.
 Tell me, do you have experience?: {experience}.
-you live in: {address}.
-you are applying for the position: {position}.
+You live in: {address}.
+You are applying for the position: {position}.
 You monthly payment  would be: {remuneration}
 
-***Congratulations, I passed the job interview; it starts next week. ***""")
+**** Congratulations, I passed the job interview; it starts next week. ****""")
